d46daa0b9e7b39ff9ea48ae5d8705c65b1fe5bdb
Ryon Bazzle <ryonbazzle@Ryons-MacBook-Pro.local>
2019-11-13 13:10:50 -0700

    data from github now, not JSONbin

--- code.js
+++ code.js
@@ -8,14 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 figma.showUI(__html__, { visible: false });
-figma.ui.postMessage({ type: 'networkRequest' });
-figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
+figma.ui.postMessage({ type: "networkRequest" });
+figma.loadFontAsync({ family: "Roboto", style: "Regular" });
 let styleList = figma.getLocalPaintStyles();
 // styleList.forEach(element => {
 // 	element.remove();
 // });
-console.log(figma.root);
 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    console.log(msg);
     msg.forEach(element => {
         let { name, rgb, opacity, description, theme, type, active } = element;
         if (theme == figma.currentPage.name && active) {
@@ -24,7 +24,7 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
             style.name = `${type}/${name}`;
             style.paints = [
                 {
-                    type: 'SOLID',
+                    type: "SOLID",
                     color: { r, g, b },
                     opacity
                 }
@@ -33,7 +33,7 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
         }
     });
     let styleList = figma.getLocalPaintStyles();
-    console.log(styleList);
+    //   console.log(styleList);
     const nodes = [];
     //CREATE RECTANGLES/CIRCLES WITH LABELS
     for (let i = 0; i < styleList.length; i++) {
@@ -45,15 +45,15 @@ figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
         rect.fillStyleId = id;
         figma.currentPage.appendChild(rect);
         const label = figma.createText();
-        label.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+        label.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
         label.characters = name;
         label.fontSize = 30;
         label.x = 200;
         label.y = i * 150;
-        label.textAlignVertical = 'TOP';
+        label.textAlignVertical = "TOP";
         label.lineHeight = {
             value: 100,
-            unit: 'PIXELS'
+            unit: "PIXELS"
         };
         label.name = name;
         figma.currentPage.appendChild(label);
--- code.ts
+++ code.ts
@@ -1,65 +1,65 @@
 figma.showUI(__html__, { visible: false });
-figma.ui.postMessage({ type: 'networkRequest' });
-figma.loadFontAsync({ family: 'Roboto', style: 'Regular' });
+figma.ui.postMessage({ type: "networkRequest" });
+figma.loadFontAsync({ family: "Roboto", style: "Regular" });
 let styleList = figma.getLocalPaintStyles();
 // styleList.forEach(element => {
 // 	element.remove();
 // });
-console.log(figma.root);
 figma.ui.onmessage = async msg => {
-	msg.forEach(element => {
-		let { name, rgb, opacity, description, theme, type, active } = element;
-		if (theme == figma.currentPage.name && active) {
-			let { r, g, b } = rgb;
+  console.log(msg);
+  msg.forEach(element => {
+    let { name, rgb, opacity, description, theme, type, active } = element;
+    if (theme == figma.currentPage.name && active) {
+      let { r, g, b } = rgb;
 
-			const style = figma.createPaintStyle();
-			style.name = `${type}/${name}`;
-			style.paints = [
-				{
-					type: 'SOLID',
-					color: { r, g, b },
-					opacity
-				}
-			];
-			style.description = description;
-		}
-	});
-	let styleList = figma.getLocalPaintStyles();
-	console.log(styleList);
-	const nodes: SceneNode[] = [];
+      const style = figma.createPaintStyle();
+      style.name = `${type}/${name}`;
+      style.paints = [
+        {
+          type: "SOLID",
+          color: { r, g, b },
+          opacity
+        }
+      ];
+      style.description = description;
+    }
+  });
+  let styleList = figma.getLocalPaintStyles();
+  //   console.log(styleList);
+  const nodes: SceneNode[] = [];
 
-	//CREATE RECTANGLES/CIRCLES WITH LABELS
+  //CREATE RECTANGLES/CIRCLES WITH LABELS
 
-	for (let i = 0; i < styleList.length; i++) {
-		const { name, id } = styleList[i];
+  for (let i = 0; i < styleList.length; i++) {
+    const { name, id } = styleList[i];
 
-		const rect = figma.createRectangle();
-		rect.name = name;
-		rect.y = i * 150;
-		rect.cornerRadius = 100;
-		rect.fillStyleId = id;
-		figma.currentPage.appendChild(rect);
+    const rect = figma.createRectangle();
+    rect.name = name;
+    rect.y = i * 150;
+    rect.cornerRadius = 100;
+    rect.fillStyleId = id;
+    figma.currentPage.appendChild(rect);
 
-		const label = figma.createText();
-		label.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-		label.characters = name;
-		label.fontSize = 30;
-		label.x = 200;
-		label.y = i * 150;
-		label.textAlignVertical = 'TOP';
-		label.lineHeight = {
-			value: 100,
-			unit: 'PIXELS'
-		};
-		label.name = name;
-		figma.currentPage.appendChild(label);
+    const label = figma.createText();
+    label.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
+    label.characters = name;
+    label.fontSize = 30;
+    label.x = 200;
+    label.y = i * 150;
+    label.textAlignVertical = "TOP";
+    label.lineHeight = {
+      value: 100,
+      unit: "PIXELS"
+    };
+    label.name = name;
+    figma.currentPage.appendChild(label);
 
-		nodes.push(rect, label);
-	}
+    nodes.push(rect, label);
+  }
 
-	figma.currentPage.selection = nodes;
-	figma.viewport.scrollAndZoomIntoView(nodes);
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
 
-	//CLOSE PLUGIN
-	figma.closePlugin();
+  //CLOSE PLUGIN
+  figma.closePlugin();
 };
