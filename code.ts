8e348d4d983d546c4f752782b15ffd9eb2fbc004
Ryon Bazzle <ryonbazzle@Ryons-MacBook-Pro.local>
2019-11-15 01:38:38 -0700

    fixed for now dark mode overwrite issue

--- code.ts
+++ code.ts
@@ -29,98 +29,102 @@ function clone(val) {
   throw "unknown";
 }
 function addStyle(color) {
-  let { type, rgb, name, opacity, theme, description, active } = color;
-  if (theme == figma.currentPage.name && active) {
-    let { r, g, b } = rgb;
+  let { type, rgb, name, opacity, theme, description } = color;
+  let { r, g, b } = rgb;
 
-    const style = figma.createPaintStyle();
-    style.name = `${type}/${name}`;
-    style.paints = [
-      {
-        type: "SOLID",
-        color: { r, g, b },
-        opacity
-      }
-    ];
-    style.description = `${description} (${figma.currentPage.name})`;
-    console.log("ADDED");
-  } else {
-    console.log("PAGE NAME DOES NOT MATCH");
-  }
+  const style = figma.createPaintStyle();
+  style.name = `${type}/${name}`;
+  style.paints = [
+    {
+      type: "SOLID",
+      color: { r, g, b },
+      opacity
+    }
+  ];
+  style.description = `${description} (${theme})`;
+  console.log("ADDED");
 }
-function updateStyle(color, style) {
-  let { rgb, opacity } = color;
-  // console.log(rgb);
-  const fills = clone(style.paints);
-  fills[0].color = rgb;
-  fills[0].opacity = opacity;
-  style.paints = fills;
-  console.log(style);
-  console.log("UPDATED");
+function updateStyle(color, style, page) {
+  console.log(style.name, style.description, page);
+  if (style.description == page) {
+    let { rgb, opacity } = color;
+    const fills = clone(style.paints);
+    fills[0].color = rgb;
+    fills[0].opacity = opacity;
+    style.paints = fills;
+    console.log(`UPDATED ${style.name} with ${color.name}-${color.theme}`);
+  } else {
+    console.log("MATCH WRONG");
+  }
 }
 function deleteStyle(style) {
   style.remove;
 }
 
+//CREATE RECTANGLES/CIRCLES WITH LABELS
+function styleGuide(style, i, nodes) {
+  let { name, id } = style;
+  const rect = figma.createRectangle();
+  rect.name = name;
+  rect.y = i * 150;
+  rect.cornerRadius = 100;
+  rect.fillStyleId = id;
+  figma.currentPage.appendChild(rect);
+
+  const label = figma.createText();
+  label.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
+  label.characters = name;
+  label.fontSize = 30;
+  label.x = 200;
+  label.y = i * 150;
+  label.textAlignVertical = "TOP";
+  label.lineHeight = {
+    value: 100,
+    unit: "PIXELS"
+  };
+  label.name = name;
+  figma.currentPage.appendChild(label);
+
+  nodes.push(rect, label);
+}
+
 figma.ui.onmessage = async msg => {
   let styleList = figma.getLocalPaintStyles();
   const styleLength = styleList.length;
-  if (styleLength == 0) {
+  let page = figma.currentPage.name;
+  const nodes: SceneNode[] = [];
+
+  if (styleList.length == 0) {
     msg.forEach(color => {
-      addStyle(color);
+      if (color.active) {
+        addStyle(color);
+      }
     });
   } else {
     msg.forEach(element => {
       for (let i = 0; i < styleLength; i++) {
-        let { name: styleName, description: styleDesc } = styleList[i];
         let { name, theme } = element;
+        let { name: styleName, description: styleDesc } = styleList[i];
         let styleName1 = styleName.split("/");
         styleName = styleName1[1].toString();
-        let regex = new RegExp(`(${theme})`, "g");
 
-        if (name == styleName && styleDesc.match(theme)) {
-          updateStyle(element, styleList[i]);
-          console.log(`MATCHED ${element.name}`);
-        } else if (name == styleName && !styleDesc.match(regex)) {
-          console.log(`IGNORED ${element.name}-${element.theme}`);
+        if (
+          element.name == styleName &&
+          styleDesc.match(new RegExp(`(${page})`, "g"))
+        ) {
+          console.log(`MATCH ${name}-${theme}`);
+
+          //passing in just the name, so it changes the first instance of that name
+          updateStyle(element, styleList[i], page);
+          styleGuide(styleList[i], i, nodes);
+        } else {
+          console.log("NOT A MATCH");
         }
       }
     });
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
   }
-
-  const nodes: SceneNode[] = [];
-
-  //CREATE RECTANGLES/CIRCLES WITH LABELS
-
-  for (let i = 0; i < styleList.length; i++) {
-    const { name, id } = styleList[i];
-
-    const rect = figma.createRectangle();
-    rect.name = name;
-    rect.y = i * 150;
-    rect.cornerRadius = 100;
-    rect.fillStyleId = id;
-    figma.currentPage.appendChild(rect);
-
-    const label = figma.createText();
-    label.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
-    label.characters = name;
-    label.fontSize = 30;
-    label.x = 200;
-    label.y = i * 150;
-    label.textAlignVertical = "TOP";
-    label.lineHeight = {
-      value: 100,
-      unit: "PIXELS"
-    };
-    label.name = name;
-    figma.currentPage.appendChild(label);
-
-    nodes.push(rect, label);
-  }
-
-  figma.currentPage.selection = nodes;
-  figma.viewport.scrollAndZoomIntoView(nodes);
 
   //CLOSE PLUGIN
   figma.closePlugin();
